639d42f433081a6a07fab5e785e726f6bb2c8251
kkossev <kossevkrassimir@gmail.com>
2021-10-23 18:48:34 +0300

    Create Hubitat Zigbee Lab.groovy

---
+++ Drivers/Hubitat Zigbee Lab/Hubitat Zigbee Lab.groovy
@@ -0,0 +1,18 @@
+/**
+ *  Hubitat Zigbee Lab driver.
+ *  This is a HE driver to consolidate all my experimental Zigbee protocol bits and pieces from my production drivers into a single test driver code.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ * 
+ *  The inital version was based on ST DH "Zemismart Button", namespace: SangBoy, author: YooSangBeom
+ * 
+ * ver. 1.0.0 2021-10-23 kkossev  - first dummy version
+ *
+ */
