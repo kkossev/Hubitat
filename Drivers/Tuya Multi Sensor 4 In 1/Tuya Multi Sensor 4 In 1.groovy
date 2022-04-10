8d628cf87d11885d4a8669c8e2cf5fc6c101d116
kkossev <kossevkrassimir@gmail.com>
2022-04-10 15:42:52 +0300

    Create Tuya Multi Sensor 4 In 1.groovy

---
+++ Drivers/Tuya Multi Sensor 4 In 1/Tuya Multi Sensor 4 In 1.groovy
@@ -0,0 +1,18 @@
+/**
+ *  Tuya Multi Sensor 4 In 1 driver for Hubitat
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ * 
+ * ver. 1.0.0 2022-04-10 kkossev  - Inital test version
+ *
+*/
+
+def version() { "1.0.0" }
+def timeStamp() {"2022/04/10 3:42 PM"}
