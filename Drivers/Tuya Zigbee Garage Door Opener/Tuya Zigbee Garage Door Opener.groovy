61de84a689aec99819f7cb7929a17003ef5d0911
kkossev <kossevkrassimir@gmail.com>
2022-06-18 00:09:28 +0300

    Tuya Zigbee Garage Door Opener 1.0.0

--- Drivers/Tuya Zigbee Garage Door Opener/Tuya Zigbee Garage Door Opener.groovy
+++ Drivers/Tuya Zigbee Garage Door Opener/Tuya Zigbee Garage Door Opener.groovy
@@ -12,9 +12,219 @@
  *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *	for the specific language governing permissions and limitations under the License.
  * 
- * ver. 1.0.0 2022-06-17 kkossev  - Inital test version
+ * ver. 1.0.0 2022-06-18 kkossev  - Inital test version
  *
 */
 
 def version() { "1.0.0" }
-def timeStamp() {"2022/06/17 11:06 AM"}
+def timeStamp() {"2022/06/18 12:03 AM"}
+
+import hubitat.device.HubAction
+import hubitat.device.Protocol
+import groovy.transform.Field
+import hubitat.zigbee.zcl.DataType
+
+@Field static final Boolean debug = false
+@Field static final Integer pulseTimer  = 1000
+@Field static final Integer doorTimeout = 5000
+
+
+metadata {
+    definition (name: "Tuya Zigbee Garage Door Opener", namespace: "kkossev", author: "Krassimir Kossev", importUrl: "https://raw.githubusercontent.com/kkossev/Hubitat/development/Drivers/Tuya%20Zigbee%20Metering%20Plug/Tuya%20Zigbee%20Metering%20Plug", singleThreaded: true ) {
+        capability "Actuator"
+        capability "GarageDoorControl"    // door - ENUM ["unknown", "open", "closing", "closed", "opening"]; Commands: close() open()
+        capability "ContactSensor"        // contact - ENUM ["closed", "open"]
+        capability "Configuration"
+        capability "Switch"
+        
+        if (debug) {
+            command "initialize", [[name: "Manually initialize the device after switching drivers.  \n\r     ***** Will load device default values! *****" ]]
+        }
+        
+        fingerprint profileId:"0104", model:"TS0601", manufacturer:"_TZE200_wfxuhoea", endpointId:"01", inClusters:"0000,0004,0005,EF00", outClusters:"0019,000A", application:"42", deviceJoinName: "Tuya Zigbee Garage Door Opener"
+    }
+
+    preferences {
+        input (name: "logEnable", type: "bool", title: "<b>Debug logging</b>", description: "<i>Debug information, useful for troubleshooting. Recommended value is <b>false</b></i>", defaultValue: true)
+        input (name: "txtEnable", type: "bool", title: "<b>Description text logging</b>", description: "<i>Display measured values in HE log page. Recommended value is <b>true</b></i>", defaultValue: true)
+    }
+}
+
+
+private getCLUSTER_TUYA() { 0xEF00 }
+
+// Parse incoming device messages to generate events
+def parse(String description) {
+    if (logEnable == true) log.debug "${device.displayName} parse: description is $description"
+    checkDriverVersion()
+	if (description?.startsWith('catchall:') || description?.startsWith('read attr -')) {
+        def descMap = [:]
+        try {
+            descMap = zigbee.parseDescriptionAsMap(description)
+        }
+        catch (e) {
+            log.warn "${device.displayName} parse: exception caught while parsing descMap:  ${descMap}"
+            return null
+        }
+		if (descMap?.clusterInt == CLUSTER_TUYA) {
+        	if (logEnable) log.debug "${device.displayName} parse Tuya Cluster: descMap = $descMap"
+			if ( descMap?.command in ["00", "01", "02"] ) {
+				def switchFunc = (descMap?.data[2])
+                def switchAttr = (descMap?.data[3])   
+                def switchState = (descMap?.data[6]) == "01" ? "on" : "off"
+                if (switchFunc in ["01", "02", "03", "04"] && switchAttr =="01") {
+   	                def cd = getChildDevice("${device.id}-${switchFunc}")
+				    if (cd == null) {
+				        return createEvent(name: "switch", value: switchState)
+				    }
+                    if (descMap?.command == "00") {
+			            // switch toggled
+			            cd.parse([[name: "switch", value:switchState, descriptionText: "Child switch ${switchFunc} turned $switchState"]])
+			        } 
+			        else if (descMap?.command in ["01", "02"]) {
+                        // report switch status
+                        cd.parse([[name: "switch", value:switchState, descriptionText: "Child switch ${switchFunc} is $switchState"]])
+			        }
+                    if (switchState == "on") {
+			            if (logEnable) log.debug "Parent Switch ON"
+			            return createEvent(name: "switch", value: "on")
+			        } 
+			        else if (switchState == "off") {
+			            def cdsOn = 0
+			            // cound number of switches on
+			            getChildDevices().each { child ->
+                            if (getChildId(child) != switchFunc && child.currentValue('switch') == "on") {
+                                cdsOn++
+                            }
+			            }
+			            if (cdsOn == 0) {
+                            if (logEnable) log.debug "Parent Switch OFF"
+                            return createEvent(name: "switch", value: "off")
+			            }    
+			        }
+                }
+			} // if command in ["00", "01", "02"]
+		} // if Tuya cluster
+	}
+}   
+
+
+def on() { 
+    if (logEnable) log.debug "${device.displayName} Turning the relay ON"
+     sendZigbeeCommands(zigbee.command(0xEF00, 0x0, "00010101000101"))
+}
+
+def off() {
+    if (logEnable) log.debug "${device.displayName} Turning the relay OFF"	
+    sendZigbeeCommands(zigbee.command(0xEF00, 0x0, "00010101000100"))
+}
+
+
+def pulseOn() {
+    if (logEnable) log.debug "${device.displayName} pulseOn()"
+    runInMillis( pulseTimer, pulseOff, [overwrite: true])
+    on()
+}
+
+def pulseOff() {
+    if (logEnable) log.debug "${device.displayName} pulseOff()"
+    off()
+}
+
+
+def open() {
+    log.debug "${device.displayName} open()"
+	sendEvent(name: "door", value: "opening")
+    runInMillis( doorTimeout, confirmOpen, [overwrite: true])
+    pulseOn()
+}
+
+def close() {
+    log.debug "${device.displayName} close()"
+	sendEvent(name: "door", value: "closing")
+    runInMillis( doorTimeout, confirmClosed, [overwrite: true])
+    pulseOn()
+}
+
+
+def confirmClosed(){
+	sendEvent(name: "door", value: "closed")
+    sendEvent(name: "contact", value: "closed")
+}
+
+def confirmOpen(){
+    sendEvent(name: "door", value: "open")
+    sendEvent(name: "contact", value: "open")
+}
+
+void initializeVars( boolean fullInit = true ) {
+    if (logEnable==true) log.info "${device.displayName} InitializeVars()... fullInit = ${fullInit}"
+    if (fullInit == true ) {
+        state.clear()
+        state.driverVersion = driverVersionAndTimeStamp()
+    }
+    if (fullInit == true || settings?.logEnable == null) device.updateSetting("logEnable", true)
+    if (fullInit == true || settings?.txtEnable == null) device.updateSetting("txtEnable", true)
+}
+
+def initialize() {
+    if (txtEnable==true) log.info "${device.displayName} Initialize()..."
+    unschedule()
+    initializeVars()
+	sendEvent(name: "door", value: "unknown")
+	sendEvent(name: "contact", value: "unknown")
+    updated()            // calls also configure()
+}
+
+void logsOff(){
+    log.warn "${device.displayName} Debug logging disabled..."
+    device.updateSetting("logEnable",[value:"false",type:"bool"])
+}
+
+def tuyaBlackMagic() {
+    return zigbee.readAttribute(0x0000, [0x0004, 0x000, 0x0001, 0x0005, 0x0007, 0xfffe], [:], delay=200)
+}
+
+def configure() {
+    if (txtEnable==true) log.info "${device.displayName} configure().."
+    List<String> cmds = []
+    cmds += tuyaBlackMagic()
+    sendZigbeeCommands(cmds)
+}
+
+def updated() {
+    log.info "${device.displayName} debug logging is: ${logEnable == true}"
+    log.info "${device.displayName} description logging is: ${txtEnable == true}"
+    if (txtEnable) log.info "${device.displayName} Updated..."
+    if (logEnable) runIn(1800, logsOff)
+
+}
+
+def installed() {
+    log.info "Installing..."
+    log.warn "Debug logging will be automatically disabled after 30 minutes!"
+    device.updateSetting("logEnable",[type:"bool",value:"true"])
+    device.updateSetting("txtEnable",[type:"bool",value:"true"])
+    if (logEnable) runIn(1800,logsOff)
+}
+
+
+
+def driverVersionAndTimeStamp() {version()+' '+timeStamp()}
+
+def checkDriverVersion() {
+    if (state.driverVersion != null && driverVersionAndTimeStamp() == state.driverVersion) {
+        //log.trace "${device.displayName} driverVersion is the same ${driverVersionAndTimeStamp()}"
+    }
+    else {
+        if (txtEnable==true) log.info "${device.displayName} updating the settings from driver version ${state.driverVersion} to ${driverVersionAndTimeStamp()}"
+        initializeVars( fullInit = false ) 
+        state.driverVersion = driverVersionAndTimeStamp()
+    }
+}
+
+void sendZigbeeCommands(List<String> cmds) {
+    if (logEnable) {log.trace "${device.displayName} sendZigbeeCommands received : ${cmds}"}
+	sendHubCommand(new hubitat.device.HubMultiAction(cmds, hubitat.device.Protocol.ZIGBEE))
+}
+
