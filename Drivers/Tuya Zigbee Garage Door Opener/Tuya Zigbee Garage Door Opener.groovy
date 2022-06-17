0fb98edf281a0197f3453a2f16f9764779f0f055
kkossev <kossevkrassimir@gmail.com>
2022-06-17 15:35:16 +0300

    Create Tuya Zigbee Garage Door Opener.groovy

---
+++ Drivers/Tuya Zigbee Garage Door Opener/Tuya Zigbee Garage Door Opener.groovy
@@ -0,0 +1,20 @@
+/**
+ *  Tuya Zigbee Garage Door Opener driver for Hubitat
+ *
+ *  https://community.hubitat.com/t/tuya-zigbee-garage-door-opener/95579
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ * 
+ * ver. 1.0.0 2022-06-17 kkossev  - Inital test version
+ *
+*/
+
+def version() { "1.0.0" }
+def timeStamp() {"2022/06/17 11:06 AM"}
