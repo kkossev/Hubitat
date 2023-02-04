9928be022be1790df28b153c88a4c6718b4335f5
kkossev <kossevkrassimir@gmail.com>
2023-02-04 21:22:26 +0200

    Device Health Status : added an option to filter battery-powered only devices;

--- Apps/Device Health Status.groovy
+++ Apps/Device Health Status.groovy
@@ -1,7 +1,7 @@
 /**
  *  Device Health Status - application for Hubitat Elevation hub
  *
- *  https://community.hubitat.com/t/devicepresent-capability-healthstatus/89774/18?u=kkossev
+ *  https://community.hubitat.com/t/project-alpha-device-health-status/111817
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -17,7 +17,7 @@
  *  ver. 1.0.0 2023-02-03 kkossev - first version: 'Light Usage Table' sample app code modification
  *  ver. 1.0.1 2023-02-03 kkossev - added powerSource, battery, model, manufacturer, driver name; added option to skip the 'capability.healthCheck' filtering;
  *  ver. 1.0.2 2023-02-03 FriedCheese2006 - Tweaks to Install Process
- *  ver. 1.0.3 2023-02-04 kkossev - importUrl; documentationLink; app version; debug and info logs options;
+ *  ver. 1.0.3 2023-02-04 kkossev - importUrl; documentationLink; app version; debug and info logs options; added controller type; added an option to filter battery-powered only devices;
  *
  *          TODO :Add the "Last Activity At" devices property in the table
  *                    Green if time less than 8 hours
@@ -29,7 +29,7 @@
 import groovy.transform.Field
 
 def version() { "1.0.4" }
-def timeStamp() {"2023/02/04 6:44 PM"}
+def timeStamp() {"2023/02/04 9:18 PM"}
 
 @Field static final Boolean debug = true
 
@@ -56,29 +56,41 @@ def mainPage() {
 	if(app.getInstallationState() == "COMPLETE") {hideDevices=true} else {hideDevices=false}
 
     dynamicPage(name: "mainPage", title: "<b>Device Health Status</b> ver. ${driverVersionAndTimeStamp()}", uninstall: true, install: true) {
- 		section("Options", hideable: true, hidden: hideDevices) {
-   			input("logEnable", "bool", title: "Debug logging.", defaultValue: false, required: false)
-   			input("txtEnable", "bool", title: "Description text logging.", defaultValue: false, required: false)
-			input name: "filterHealthCheckOnly", type: "bool", title: "Show only devices that have 'Healtch Check' capability", submitOnChange: true, defaultValue: false
-   		}            
 		section("Device Selection", hideable: true, hidden: hideDevices) {
 			input name:"devices", type: filterHealthCheckOnly ? "capability.healthCheck" : "capability.*", title: "Select devices w/ <b>Health Status</b> attribute", multiple: true, submitOnChange: true, width: 4
+            logDebug "Device Selection : start"
 			devices.each {dev ->
 				if(!state.devices["$dev.id"]) {
-					state.devices["$dev.id"] = [
-						healthStatus: dev.currentValue("healthStatus"), 
-                    ]
-					state.devicesList += dev.id
-				}
-			}
+                    //logDebug "Device Selection : new device ${state.devices["$dev.id"]}" 
+                }
+                else {
+                    //logDebug "Device Selection : existing device ${state.devices["$dev.id"]}" 
+                }
+                def devData = dev.getData()              // [model:TS0601, application:44, manufacturer:_TZE200_ikvncluo]
+                def devType = dev.getTypeName()
+                def hasBattery = dev.capabilities.find { it.toString().contains('Battery') }  ? true : false
+        		state.devices["$dev.id"] = [
+		    		healthStatus: dev.currentValue("healthStatus"), 
+                    hasBattery: hasBattery
+                ]
+			    state.devicesList += dev.id
+            }
 			if(devices) {
 				if(devices.id.sort() != state.devicesList.sort()) { //something was removed
+                    logDebug "Device Selection : something was changed" 
 					state.devicesList = devices.id
 					Map newState = [:]
 					devices.each{d ->  newState["$d.id"] = state.devices["$d.id"]}
 					state.devices = newState
 				}
+                else {
+                    logDebug "Device Selection : no changes" 
+                }
 			}
+            else {
+                logWarn "Device Selection : devices = ${devices}" 
+            }
+            logDebug "Device Selection : start"
 		} // section "Device Selection"
         
 		if(hideDevices) {
@@ -87,6 +99,14 @@ def mainPage() {
 				paragraph displayTable()
 				input "refresh", "button", title: "Refresh Table", width: 2
 			}
+     		section("Options", hideable: true, hidden: hideDevices) {
+       			input("logEnable", "bool", title: "Debug logging.", defaultValue: false, required: false)
+       			input("txtEnable", "bool", title: "Description text logging.", defaultValue: false, required: false)
+                paragraph "<b>Device selection</b> options:"
+    			input name: "filterHealthCheckOnly", type: "bool", title: "Show only devices that have 'Healtch Check' capability", submitOnChange: true, defaultValue: false
+                paragraph "<b>Table</b> display options:"
+    			input name: "hideNotBatteryDevices", type: "bool", title: "Hide the <b>not</b> battery-powered devices", submitOnChange: true, defaultValue: false
+       		}            
 		} else {
 			section("CLICK DONE TO INSTALL APP AFTER SELECTING DEVICES") {
 				paragraph ""
@@ -102,28 +122,35 @@ String displayTable() {
         
 		"<thead><tr style='border-bottom:2px solid black'><th style='border-right:2px solid black'>Device</th>" +
     		"<th>healthStatus</th>"  +
-    		"<th>powerSource</th>"  +
     		"<th>battery</th>"  +
+    		"<th>powerSource</th>"  +
     		"<th>model</th>"  +
     		"<th>manufacturer</th>"  +
+    		"<th>type</th>"  +
     		"<th>driver</th>"  +
         "</tr></thead>"
     
 	devices.sort{it.displayName.toLowerCase()}.each {dev ->
-		String devLink = "<a href='/device/edit/$dev.id' target='_blank' title='Open Device Page for $dev'>$dev"
-        def healthColor = dev.currentHealthStatus == null ? "black" : dev.currentHealthStatus == "online" ? "green" : "red"
-        def healthStatus = dev.currentHealthStatus ?: "n/a"
-        def devData = dev.getData()              // [model:TS0601, application:44, manufacturer:_TZE200_ikvncluo]
+        def devData = dev.getData()
         def devType = dev.getTypeName()
-            
-		str += "<tr style='color:black'><td style='border-right:2px solid black'>$devLink</td>" +
-			"<td style='color:${healthColor}'>$healthStatus</td>" +
-            "<td style='color:${black}'>${dev.currentpowerSource ?: "n/a"}</td>" +
-            "<td style='color:${black}'>${dev.currentBattery ?: "n/a"}</td>" +
-            "<td style='color:${black}'>${devData.model ?: "n/a"}</td>" +
-            "<td style='color:${black}'>${devData.manufacturer ?: "n/a"}</td>" +
-            "<td style='color:${black}'>${devType ?: "n/a"}</td>" // +
-	}
+        if (settings?.hideNotBatteryDevices == true && state.devices["$dev.id"].hasBattery == false) {
+            //logDebug "SKIPPING dev.id=${dev.id} hasBattery = ${state.devices["$dev.id"].hasBattery}"
+        }
+        else {
+    		String devLink = "<a href='/device/edit/$dev.id' target='_blank' title='Open Device Page for $dev'>$dev"
+            def healthColor = dev.currentHealthStatus == null ? "black" : dev.currentHealthStatus == "online" ? "green" : "red"
+            def healthStatus = dev.currentHealthStatus ?: "n/a"
+                
+    		str += "<tr style='color:black'><td style='border-right:2px solid black'>$devLink</td>" +
+    			"<td style='color:${healthColor}'>$healthStatus</td>" +
+                "<td style='color:${black}'>${dev.currentBattery ?: "n/a"}</td>" +
+                "<td style='color:${black}'>${dev.currentpowerSource ?: "n/a"}</td>" +
+                "<td style='color:${black}'>${devData.model ?: "n/a"}</td>" +
+                "<td style='color:${black}'>${devData.manufacturer ?: "n/a"}</td>" +
+                "<td style='color:${black}'>${dev.controllerType ?: "n/a"}</td>" +
+                "<td style='color:${black}'>${devType ?: "n/a"}</td>" // +
+        }
+	} // for each device
 	str += "</table></div>"
 	str
 }
@@ -158,6 +185,7 @@ void healthStatusOfflineHandler(evt) {
 def driverVersionAndTimeStamp() {version()+' '+timeStamp().split(" ")[0]}
 
 def logDebug(msg) { if (logEnable) log.debug(msg) }
+def logWarn(msg)  { if (logEnable) log.warn(msg) }
 def logInfo(msg)  { if (txtEnable) log.info(msg) }
 
 def updated() {
