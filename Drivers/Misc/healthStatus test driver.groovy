0073d3377c8931f76c7438ade4925424264a3768
kkossev <kossevkrassimir@gmail.com>
2023-02-05 00:37:25 +0200

    healthStatus test driver : ver. 1.0.2 2023-02-04 kkossev  - healthStatus enum extended w/ 'unknown'; powerSource attribute must be explicitely defined in order to be used in an app!; added dummy ping()

--- Drivers/Misc/healthStatus test driver.groovy
+++ Drivers/Misc/healthStatus test driver.groovy
@@ -14,6 +14,7 @@
  *
  * ver. 1.0.0 2023-01-27 kkossev  - inital version
  * ver. 1.0.1 2023-02-02 kkossev  - a dummy capability 'Health Check' already exists in Hubitat! (unfortunately, the attribute 'healthStatus' still has to be declared)
+ * ver. 1.0.2 2023-02-04 kkossev  - healthStatus enum extended w/ 'unknown'; powerSource attribute must be explicitely defined in order to be used in an app!; added dummy ping()
  *
 */
 
@@ -22,7 +23,8 @@ metadata {
         capability "Sensor"
         capability "Health Check"
         
-        attribute "healthStatus", "enum", ["offline", "online"]
+        attribute "healthStatus", "enum", ["offline", "online", "unknown"]
+        attribute "powerSource", "enum", ["battery", "dc", "mains", "unknown"]
         
         command "offline", [[name: "Set healthStatus offline"]]
         command "online",  [[name: "Set healthStatus online" ]]
@@ -36,10 +38,16 @@ def parse(String description) {
     log.debug "${device.displayName} parse: description is $description"
 }
 
+def ping() {
+    log.debug "ping() is not implemented"
+}
+
 def offline() {
     sendEvent(name: "healthStatus", value: "offline", type: "digital", isStateChange: true )
+    sendEvent(name: "powerSource",  value: "unknown", type: "digital", isStateChange: true )
 }
 
 def online() {
     sendEvent(name: "healthStatus", value: "online", type: "digital", isStateChange: true )
+    sendEvent(name: "powerSource",  value: "battery", type: "digital", isStateChange: true )
 }
