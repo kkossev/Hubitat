b1d40ed89fcd06509b650a66d7f5320041d1d6ec
kkossev <kossevkrassimir@gmail.com>
2023-05-08 10:53:28 +0300

    Create Tuya Zigbee Device.groovy

---
+++ Drivers/Tuya Device Driver/Tuya Zigbee Device.groovy
@@ -0,0 +1,20 @@
+/**
+ * Tuya-Zigbee-Device-Driver for Hubitat
+ *
+ *  https://community.hubitat.com/t/dynamic-capabilities-commands-and-attributes-for-drivers/98342
+ *
+ * 	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * 	in compliance with the License. You may obtain a copy of the License at:
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * 	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * 	for the specific language governing permissions and limitations under the License.
+ *
+ * This driver is inspired by @w35l3y work on Tuya device driver (Edge project).
+ * For a big portions of code all credits go to Jonathan Bradshaw.
+ *
+ * ver. 2.0.0  2023-05-08 kkossev  - Initial test version
+ *
+ */
