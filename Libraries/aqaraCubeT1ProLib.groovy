4e583ba0415700f376b735bd1c5b89d16a0ebb14
kkossev <kossevkrassimir@gmail.com>
2023-07-14 07:57:17 +0300

    aqaraCubeT1ProLib 1.0.0 2023/07/14 7:56 AM - first succesful pairing !!!

--- Libraries/aqaraCubeT1ProLib.groovy
+++ Libraries/aqaraCubeT1ProLib.groovy
@@ -1,3 +1,4 @@
+
 library (
     base: "driver",
     author: "Krassimir Kossev",
@@ -28,15 +29,15 @@ library (
 
 
 def aqaraCubeT1ProLibVersion()   {"1.0.0"}
-def aqaraCubeT1ProLibTimeStamp() {"2023/07/13 1:03 PM"}
+def aqaraCubeT1ProLibTimeStamp() {"2023/07/14 7:56 AM"}
 
 metadata {
     attribute "mode", "enum", AqaraCubeModeOpts.options.values() as List<String>
     attribute "action", "enum", (AqaraCubeSceneModeOpts.options.values() + AqaraCubeActionModeOpts.options.values()) as List<String>
     attribute "cubeSide", "enum", AqaraCubeSideOpts.options.values() as List<String>
-    attribute "angle", "number"     
-        
-    fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0003,0500,0001", outClusters:"0019", model:"lumi.remote.cagl02", manufacturer:"LUMI", deviceJoinName: "Aqara Cube T1 Pro"
+    attribute "angle", "number"
+	 
+    fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0003,0001,0012,0006", outClusters:"0000,0003,0019", model:"lumi.remote.cagl02", manufacturer:"LUMI", deviceJoinName: "Aqara Cube T1 Pro"
     preferences {
         input name: 'cubeMode', type: 'enum', title: '<b>Mode</b>', options: AqaraCubeModeOpts.options, defaultValue: AqaraCubeModeOpts.defaultValue, required: true, description: '<i>Operation Mode.<br>Press LINK button 5 times to toggle between action mode and scene mode</i>'
     }
@@ -111,24 +112,53 @@ def initVarsAqaraCube(boolean fullInit=false) {
 
 def configureDeviceAqaraCube() {
     List<String> cmds = []
+    cmds += ["he raw 0x${device.deviceNetworkId} 0 0 0x8002 {40 00 00 00 00 40 8f 5f 11 52 52 00 41 2c 52 00 00} {0x0000}", "delay 50",]
+
+        cmds += zigbee.writeAttribute(0xFCC0, 0x0009, 0x20, 1 /*mode*/, [mfgCode: 0x115F], delay=200)
+
+    
+    // https://github.com/Koenkk/zigbee-herdsman-converters/pull/5367
+  //cmds += ["he raw 0x${device.deviceNetworkId} 1 ${device.endpointId} 0xFCC0 {14 5F 11 01 02 FF 00 41 10 02 32 71 76 20 79 16 48 28 87 18 12 21 55 72 36}  {0x0104}", "delay 50",]      // FP1 write attr 0xFF 16 bytes
+    cmds += ["he raw 0x${device.deviceNetworkId} 1 ${device.endpointId} 0xFCC0 {14 5F 11 01 02 FF 00 41 10 45 65 21 20 75 38 17 69 78 53 89 51 13 16 49 58}  {0x0104}", "delay 50",]      // FP1 write attr 0xFF 16 bytes
+    /*
+      0x00ff: {
+        value: [0x45, 0x65, 0x21, 0x20, 0x75, 0x38, 0x17, 0x69, 0x78, 0x53, 0x89, 0x51, 0x13, 0x16, 0x49, 0x58],
+        type: 0x41
+      }
+*/
+    
+    
+    
     
     // await endpoint.write('aqaraOpple', {'mode': 1}, {manufacturerCode: 0x115f});
     // TODO !!!!
     def mode = settings?.cubeMode ?: 0
-    cmds += zigbee.writeAttribute(0xFCC0, 0x0148, 0x20, mode, [mfgCode: 0x115F], delay=200)
-    cmds += zigbee.readAttribute(0xFCC0, 0x0148, [mfgCode: 0x115F], delay=200)
-    
+ //   cmds += zigbee.writeAttribute(0xFCC0, 0x0009, 0x20, 1 /*mode*/, [mfgCode: 0x115F], delay=200)
     // TODO !!!
     //    await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic','genOnOff','genPowerCfg','genMultistateInput']);
+ 
+    cmds += ["zdo bind 0x${device.deviceNetworkId} 0x01 0x01 0x0000 {${device.zigbeeId}} {}", "delay 251", ]
+    cmds += ["zdo bind 0x${device.deviceNetworkId} 0x01 0x01 0x0006 {${device.zigbeeId}} {}", "delay 251", ]
+    cmds += ["zdo bind 0x${device.deviceNetworkId} 0x01 0x01 0x0001 {${device.zigbeeId}} {}", "delay 251", ]
+//    cmds += ["zdo bind 0x${device.deviceNetworkId} 0x01 0x01 0x0012 {${device.zigbeeId}} {}", "delay 251", ]
+    
+//        cmds += ["zdo bind 0x${device.deviceNetworkId} 0x01 0x02 0x0012 {${device.zigbeeId}} {}", "delay 251", ]
+//        cmds += ["zdo bind 0x${device.deviceNetworkId} 0x01 0x03 0x000C {${device.zigbeeId}} {}", "delay 251", ]
 
+    
+    cmds += zigbee.readAttribute(0xFCC0, 0x0009, [mfgCode: 0x115F], delay=200)
+    
     // TODO !!!
     //    await endpoint.read('genPowerCfg', ['batteryVoltage']);
+    cmds += zigbee.readAttribute(0x0001, 0x0020, [:], delay=200)
 
     // TODO !!!
     //    await endpoint.read('aqaraOpple', [0x0148], {manufacturerCode: 0x115f});
+    cmds += zigbee.readAttribute(0xFCC0, 0x0148, [mfgCode: 0x115F], delay=200)   
 
     // TODO !!!
     //    await endpoint.read('aqaraOpple', [0x0149], {manufacturerCode: 0x115f});
+    cmds += zigbee.readAttribute(0xFCC0, 0x0149, [mfgCode: 0x115F], delay=200)   
     
     logDebug "configureDeviceAqaraCube() : ${cmds}"
     return cmds    
@@ -199,6 +229,51 @@ const definition = {
    if (result && !utils.isLegacyEnabled(options)) { delete result.to_side, delete result.from_side };
    return result ? result : null;
  },
+
+
+
+
+*/
+
+
+/*
+
+Manufacturer:	LUMI
+Endpoint 01 application:	19
+Endpoint 01 endpointId:	01
+Endpoint 01 idAsInt:	1
+Endpoint 01 inClusters:	0000,0003,0001,0012,0006
+Endpoint 01 initialized:	true
+Endpoint 01 manufacturer:	LUMI
+Endpoint 01 model:	lumi.remote.cagl02
+Endpoint 01 outClusters:	0000,0003,0019
+Endpoint 01 profileId:	0104
+Endpoint 01 stage:	4
+
+
+Endpoint 02 application:	unknown
+Endpoint 02 endpointId:	02
+Endpoint 02 idAsInt:	2
+Endpoint 02 inClusters:	0012
+Endpoint 02 initialized:	true
+Endpoint 02 manufacturer:	unknown
+Endpoint 02 model:	unknown
+Endpoint 02 outClusters:	0012
+Endpoint 02 profileId:	0104
+Endpoint 02 stage:	4
+
+
+Endpoint 03 application:	unknown
+Endpoint 03 endpointId:	03
+Endpoint 03 idAsInt:	3
+Endpoint 03 inClusters:	000C
+Endpoint 03 initialized:	true
+Endpoint 03 manufacturer:	unknown
+Endpoint 03 model:	unknown
+Endpoint 03 outClusters:	000C
+Endpoint 03 profileId:	0104
+Endpoint 03 stage:	4
+
 
 
 
