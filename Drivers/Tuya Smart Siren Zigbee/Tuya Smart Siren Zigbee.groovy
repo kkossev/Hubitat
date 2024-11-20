2cdaa89f3b8eaa230db4be79b4f88186c5ea1027
kkossev <kossevkrassimir@gmail.com>
2024-11-20 08:23:11 +0200

    Tuya Smart Siren Zigbee ver. 1.3.3 2024-11-20 kkossev  - (dev.branch) added TS0601 _TZE204_hcxvyxa5 and _TZE204_q76rtoa9 in Tuya group;

--- Drivers/Tuya Smart Siren Zigbee/Tuya Smart Siren Zigbee.groovy
+++ Drivers/Tuya Smart Siren Zigbee/Tuya Smart Siren Zigbee.groovy
@@ -23,7 +23,8 @@
  * ver. 1.2.2 2023-07-19 kkossev  - fix: moved _TZE204_t1blo2bj in Tuya group;
  * ver. 1.3.0 2024-10-07 kkossev  - setVolume bug fix; adding Tuya Solar Alarm '_TZE200_nlrfgpny', '_TZE204_nlrfgpny'
  * ver. 1.3.1 2024-10-08 kkossev  - restored the overwritten code changes in the previous version;
- * ver. 1.3.2 2024-10-09 kkossev  - (dev.branch)  debug enabled;  added Refresh() command; setMelody(); setDuration(); stop(); both(); siren(); strobe();
+ * ver. 1.3.2 2024-10-09 kkossev  - debug enabled;  added Refresh() command; setMelody(); setDuration(); stop(); both(); siren(); strobe();
+ * ver. 1.3.3 2024-11-20 kkossev  - (dev.branch) added TS0601 _TZE204_hcxvyxa5 and _TZE204_q76rtoa9 in Tuya group;
  *
  *                                  TODO: add ping() and healthStatus
  *                                  TODO: add TS0216  _TYZB01_0wcfvptl https://github.com/zigpy/zha-device-handlers/issues/1824#issuecomment-1302637169 (https://community.hubitat.com/t/release-tuya-smart-siren-zigbee-driver/91772/74?u=kkossev)
@@ -33,8 +34,8 @@
  *
 */
 
-def version() { "1.3.2" }
-def timeStamp() {"2024/10/09 7:59 PM"}
+def version() { "1.3.3" }
+def timeStamp() {"2024/11/20 8:12 AM"}
 
 import groovy.json.*
 import groovy.transform.Field
@@ -98,7 +99,9 @@ metadata {
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0004,0005,EF00,0000", outClusters:"0019,000A" ,model:"TS0601", manufacturer:"_TZE204_t1blo2bj", deviceJoinName: "Tuya Smart Siren"              // https://community.hubitat.com/t/release-tuya-smart-siren-zigbee-driver/91772/67?u=kkossev
         // https://github.com/zigpy/zha-device-handlers/issues/1379#issuecomment-1077772021 
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0004,0005,EF00,0000,ED00", outClusters:"0019,000A" ,model:"TS0601", manufacturer:"_TZE284_nlrfgpny", deviceJoinName: "Tuya Solar Alarm"
-        fingerprint profileId:"0104", endpointId:"01", inClusters:"0004,0005,EF00,0000,ED00", outClusters:"0019,000A" ,model:"TS0601", manufacturer:"_TZE200_nlrfgpny", deviceJoinName: "Tuya Solar Alarm"          // https://community.hubitat.com/t/release-tuya-smart-siren-zigbee-driver/91772/116?u=kkossev
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0004,0005,EF00,0000,ED00", outClusters:"0019,000A" ,model:"TS0601", manufacturer:"_TZE200_nlrfgpny", deviceJoinName: "Tuya Solar Alarm"         // https://community.hubitat.com/t/release-tuya-smart-siren-zigbee-driver/91772/116?u=kkossev
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0004,0005,EF00,0000", outClusters:"0019,000A" ,model:"TS0601", manufacturer:"_TZE204_hcxvyxa5", deviceJoinName: "Tuya Smart Siren"              // https://community.hubitat.com/t/release-tuya-smart-siren-zigbee-driver/91772/148?u=kkossev
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0004,0005,EF00,0000", outClusters:"0019,000A" ,model:"TS0601", manufacturer:"_TZE204_q76rtoa9", deviceJoinName: "Tuya Smart Siren"              // https://community.hubitat.com/t/release-tuya-smart-siren-zigbee-driver/91772/148?u=kkossev
     }
     preferences {
         input (name: "logEnable", type: "bool", title: "Debug logging", description: "<i>Debug information, useful for troubleshooting. Recommended value is <b>false</b></i>", defaultValue: true)
@@ -134,7 +137,7 @@ metadata {
 @Field static final Integer INFO_AUTO_CLEAR_PERIOD = 5      // automatically clear the Info attribute after 5 seconds
 
 def isNeo()  { (device.getDataValue("manufacturer") in ['_TZE200_d0yu2xgi', '_TZE200_d0yu2xgi', 'd0yu2xgi']) }
-def isTuya() { (device.getDataValue("manufacturer") in ['_TZE204_t1blo2bj']) }
+def isTuya() { (device.getDataValue("manufacturer") in ['_TZE204_t1blo2bj', '_TZE204_hcxvyxa5', '_TZE204_q76rtoa9']) }
 def isSolarAlarm() { (device.getDataValue("manufacturer") in ['_TZE200_nlrfgpny', '_TZE204_nlrfgpny']) }        // https://github.com/Koenkk/zigbee-herdsman-converters/blob/06c82b69d57358d0a879928deaff05305501366c/src/devices/neo.ts#L106
 
 @Field static final Map TemperatureScaleOptions = [
